e131939ae4c8d05f27bce3b510a9f4f006d8b014
snowiewdev <snowiewongdev@gmail.com>
2023-12-12 16:14:35 -0800

    lint fix

--- blocks/person-cards/person-cards.js
+++ blocks/person-cards/person-cards.js
@@ -1,14 +1,14 @@
-import { createTag, replaceAllChildElements } from "../../scripts/helpers.js";
-import { addTextSplitAnimationToAllLinks } from "../../scripts/animation.js";
+import { createTag, replaceAllChildElements } from '../../scripts/helpers.js';
+import { addTextSplitAnimationToAllLinks } from '../../scripts/animation.js';
 
 function createPersonInfo(div) {
-  const personTitles = div.querySelectorAll("h5");
+  const personTitles = div.querySelectorAll('h5');
   if (personTitles.length === 0) return null;
 
-  const personInfo = createTag("div", { class: "person-card-person-info" }, "");
+  const personInfo = createTag('div', { class: 'person-card-person-info' }, '');
   personTitles.forEach((title, index) => {
-    if (index === 0) title.classList.add("person-name", "heading-xs");
-    if (index === 1) title.classList.add("person-title", "description-s");
+    if (index === 0) title.classList.add('person-name', 'heading-xs');
+    if (index === 1) title.classList.add('person-title', 'description-s');
     personInfo.append(title);
   });
 
@@ -16,18 +16,18 @@ function createPersonInfo(div) {
 }
 
 function addPersonIconToPersonInfo(div, personInfo) {
-  const icon = div.querySelector("picture");
+  const icon = div.querySelector('picture');
   if (!icon) return;
-  const iconWrapper = createTag("div", { class: "person-info-icon" }, "");
+  const iconWrapper = createTag('div', { class: 'person-info-icon' }, '');
   const parentWrapper = icon.parentNode;
   iconWrapper.append(icon);
   parentWrapper.remove();
 
   const personInfoTextHTML = personInfo.innerHTML;
   const personTextWrapper = createTag(
-    "div",
-    { class: "person-info-text" },
-    personInfoTextHTML
+    'div',
+    { class: 'person-info-text' },
+    personInfoTextHTML,
   );
 
   replaceAllChildElements(personInfo, iconWrapper, personTextWrapper);
@@ -35,35 +35,33 @@ function addPersonIconToPersonInfo(div, personInfo) {
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    row.classList.add("person-card-wrapper");
+    row.classList.add('person-card-wrapper');
 
     [...row.children].forEach((col, index) => {
       col.classList.add(`person-card-col-${index + 1}`);
 
       // image column
       if (index === 0) {
-        col.classList.add("person-image-wrapper");
-        const images = col.querySelectorAll("img");
+        col.classList.add('person-image-wrapper');
+        const images = col.querySelectorAll('img');
         if (images.length === 2) {
-          images[0].classList.add("original-image");
-          images[1].classList.add("hover-active-image");
+          images[0].classList.add('original-image');
+          images[1].classList.add('hover-active-image');
         }
       }
 
       // content column
       if (index === 1) {
-        const title = col.querySelector("h4");
-        if (title) title.classList.add("person-card-title", "heading-s");
+        const title = col.querySelector('h4');
+        if (title) title.classList.add('person-card-title', 'heading-s');
 
-        const paragraphs = col.querySelectorAll("p");
+        const paragraphs = col.querySelectorAll('p');
         if (paragraphs.length > 0) {
-          paragraphs.forEach((p) =>
-            p.classList.add("description-m", "description")
-          );
+          paragraphs.forEach((p) => p.classList.add('description-m', 'description'));
         }
 
         const personInfo = createPersonInfo(col);
-        if (block.classList.contains("testimonial")) {
+        if (block.classList.contains('testimonial')) {
           addPersonIconToPersonInfo(col, personInfo);
         }
 

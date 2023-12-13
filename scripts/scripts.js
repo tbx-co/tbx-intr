ab0e23f18fe974e855a12b9cf2109aea79fd2546
snowiewdev <snowiewongdev@gmail.com>
2023-12-13 15:12:41 -0800

    lint fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -43,7 +43,7 @@ function addGAtagCode(gaCodeID) {
 
   const gaInitScript = createTag('script', {
   }, `
-      window.dataLayer = window.dataLayer || [];
+    window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
 

edc647ac51121e843d5b34ffc504b0b874cf3de2
snowiewdev <snowiewongdev@gmail.com>
2023-12-14 10:33:16 -0800

    added gtm code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -7,7 +7,7 @@ sampleRUM('cwv');
 
 // add more delayed functionality here
 
-// GA setup
+// GA4 setup
 const GAcodeID = 'G-X2NK8ZFC38';
 function addGAtagCode(gaCodeID) {
   const gaScriptSrc = `https://www.googletagmanager.com/gtag/js?id=${gaCodeID}`;
@@ -27,4 +27,24 @@ function addGAtagCode(gaCodeID) {
   document.body.append(gaInitScript);
 }
 
+// GTM code setup
+const GTMcodeID = 'GTM-523X9Q42';
+function addGTMtagCode(GTMcodeID) {
+    const gtmScript = createTag('script', {}, `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTMcodeID}');
+    `);
+    document.head.prepend(gtmScript);
+
+    const gtmIframe = createTag('iframe', {
+        src: `https://www.googletagmanager.com/ns.html?id=${GTMcodeID}`,
+        height: "0",
+        width: "0",
+        style: "display:none;visibility:hidden"
+    });
+    const gtmNoScript = createTag('noscript', {}, gtmIframe);
+    document.body.prepend(gtmNoScript);
+}
+
+addGTMtagCode(GTMcodeID);
 addGAtagCode(GAcodeID);

ae581a97d0dda2773d84724b365b82bc684b31a8
snowiewdev <snowiewongdev@gmail.com>
2023-12-14 11:18:17 -0800

    increase delayed time

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -202,7 +202,7 @@ function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
   window.setTimeout(() => {
     import('./delayed.js');
-  }, 3000);
+  }, 4000);
   // load anything that can be postponed to the latest here
 }
 
